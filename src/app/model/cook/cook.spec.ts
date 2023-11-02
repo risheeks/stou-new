ccb65bb3462d237eb5aee804aaa09e37027a1ce9
risheeks <risheeks@gmail.com>
2023-11-02 21:09:25 +0530

    created basic cook squares

---
+++ src/app/model/cook/cook.spec.ts
@@ -0,0 +1,7 @@
+import { Cook } from './cook';
+
+describe('Cook', () => {
+  it('should create an instance', () => {
+    expect(new Cook()).toBeTruthy();
+  });
+});
---
+++ src/app/model/cook/cook.ts
@@ -0,0 +1,8 @@
+export class Cook {
+    firstName?: string;
+    lastName?: string;
+    description?: string;
+    address?: string;
+    pincode?: number;
+    rating?: number;
+}
