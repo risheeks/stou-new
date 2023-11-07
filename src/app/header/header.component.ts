53a7a593d63f532db3869d445963684748c8d8af
Risheek Narayanadevarakere <risheeks@gmail.com>
2023-11-07 11:12:05 -0600

    Material Dialogs added **CSS incomplete**

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +12,23 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
   loggedIn?: boolean;
 
-  constructor() {
+  constructor(public dialog: MatDialog) {
     this.loggedIn = false;
+  }
+
+  openRegisterDialog() {
+    const dialogRef = this.dialog.open(RegisterComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  openLoginDialog() {
+    const dialogRef = this.dialog.open(LoginComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 }
