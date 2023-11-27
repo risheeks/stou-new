0856aed75f698e37fa559fa64598176f927f0353
Risheek Narayanadevarakere <risheeks@gmail.com>
2023-11-26 18:58:29 -0600

    logout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,6 +4,8 @@ import { MatDialog } from '@angular/material/dialog';
 import { LoginComponent } from '../login/login.component';
 import { RegisterComponent } from '../register/register.component';
 import { CookService } from '../service/cook.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-header',
@@ -13,7 +15,7 @@ import { CookService } from '../service/cook.service';
 export class HeaderComponent {
   loggedIn?: boolean;
 
-  constructor(private cookService: CookService) {
+  constructor(private cookService: CookService, private router: Router, private toastr: ToastrService) {
     this.loggedIn = false;
   }
 
@@ -21,6 +23,14 @@ export class HeaderComponent {
     this.cookService.getIsLoggedIn.subscribe(isLoggedIn => 
       this.loggedIn = isLoggedIn
     )
+  }
+
+  logout() {
+    if(this.cookService.logout()) {
+      this.router.navigate([""]);
+      this.toastr.success("logged out");
+    }
+
   }
   
 }
