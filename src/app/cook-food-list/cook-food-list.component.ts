f16db0f86a753e790637ed9ccfc495f2501292b6
Risheek Narayanadevarakere <risheeks@gmail.com>
2023-12-14 12:50:48 -0600

    New Food

--- src/app/cook-food-list/cook-food-list.component.ts
+++ src/app/cook-food-list/cook-food-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CookService } from '../service/cook.service';
+import { FoodService } from '../service/food.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-cook-food-list',
@@ -6,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./cook-food-list.component.css']
 })
 export class CookFoodListComponent {
+  foodList: any;
 
+  constructor(private cookService: CookService, private foodService: FoodService, private toastr: ToastrService) {}
+
+  ngOnInit() {
+    let cook: String = this.cookService.getLoggedInCook().username;
+    this.foodService.getFoodsByCook(cook).subscribe((foodsList: any)=> {
+      this.foodList = foodsList;
+      // console.log(this.foodList);
+    })
+  }
 }
