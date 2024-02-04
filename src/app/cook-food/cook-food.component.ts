ec5f6d5f95638fc802b99bf3d864ec990cd0d691
Risheek Narayanadevarakere <risheeks@gmail.com>
2024-02-04 12:12:01 -0600

    cook-food active

--- src/app/cook-food/cook-food.component.ts
+++ src/app/cook-food/cook-food.component.ts
@@ -2,7 +2,11 @@ import { Component, Input } from '@angular/core';
 import { CookService } from '../service/cook.service';
 import { ToastrService } from 'ngx-toastr';
 import { FoodService } from '../service/food.service';
-import { waitForAsync } from '@angular/core/testing';
+import { Food } from '../model/food/food';
+import {
+  coerceBooleanProperty,
+  BooleanInput
+} from '@angular/cdk/coercion';
 
 @Component({
   selector: 'app-cook-food',
@@ -10,12 +14,25 @@ import { waitForAsync } from '@angular/core/testing';
   styleUrls: ['./cook-food.component.css']
 })
 export class CookFoodComponent {
-  @Input() food: any;
+
+  @Input() food!: Food;
   
   constructor(private cookService: CookService, private foodService: FoodService, private toastr: ToastrService) {}
 
   ngOnInit() {
-    // console.log(this.food);
+    console.log(this.food);
+  }
+
+  getActive(): BooleanInput{
+    if(this.food.valid) return coerceBooleanProperty(this.food.valid);
+    return false;
+  }
+
+  toggleActive(active: boolean) {
+    this.food.valid = active;
+    this.foodService.updateFood(this.food).subscribe(food => {
+      this.toastr.success(this.food.title, "valid: " + this.food.valid);
+    });
   }
 
 }
